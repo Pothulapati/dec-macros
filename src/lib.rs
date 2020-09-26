554f22d756015fe7c929e0f5c5148d8974e45739
Tarun Pothulapati <tarunpothulapati@outlook.com>
2020-09-27 00:21:57 +0530

    Use Pattern Marching to get unlimited arugements
    As we can't write a macro pattern match for each additional arguement,
    we use pattern matching regex-like thing to make it expandable,
    This is done by using wrapping the input arg into a $(x),+ which means
    allow multiple of x's, where x is $element:expr
    
    As we also need to do the same internally, that is perform a push for
    each arguement, we do the same i.e $(vs.push($element);)*
    We also tackle the trailing comma problem.
    
    We also allow Vec![;] here, and learn a imp lesson of evaulating the
    arguement once and cloning it to use it multiple times, as the passed
    arg can be a single evaluation item i.e a x.take().unwrap(), etc.
    
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,32 @@ macro_rules! avec {
         Vec::new()
     };
 
+    // [3,3,4....]
     // expr covers most stuff in Rust, anything you can terminate with ;
     // Macros can't be expanded to statements directly, and hence you need to expand to a block
-    ($arg1: expr) => {{
+    // Here we are writing a regular expression to match multiple args.
+    // Here , is a token used to separate args but it could be anything as its a macro
+    // Allow, Trailing comma by adding extra $(,)? as the follow up
+    ($($args: expr),+ $(,)?) => {{
         let mut vs = Vec::new();
-        vs.push($arg1);
+        // Repeat the inside code, for each element in args
+        $(
+            vs.push($args);
+        )*
         // Return this as it will be directly returned when the macro is called, and assigned.
+        vs
+    }};
+
+    // [x ; count]
+    ($arg1: expr; $arg2: expr) => {{
+        let mut vs = Vec::new();
+        // Repeat the inside code, for each element in args
+        // we can't directly loop and call $arg1 as it can be an exp which evalualte only once
+        // Hence, evaluate it once and use clone on each push.
+        let ans = $arg1;
+        for _ in 0..$arg2 {
+            vs.push(ans.clone());
+        }
         vs
     }};
 }
@@ -31,4 +51,29 @@ fn single() {
     let x: Vec<u32> = avec!(43);
     assert!(!x.is_empty());
     assert_eq!(x[0], 43);
+}
+
+#[test]
+fn double() {
+    let x: Vec<u32> = avec!(43, 32);
+    assert!(!x.is_empty());
+    assert_eq!(x[0], 43);
+}
+
+#[test]
+fn trailing() {
+    let _ = avec!("hisdhisdisds", "asdasdasdas",);
+}
+
+#[test]
+fn clonee() {
+    let x = avec!(42; 5);
+    assert_eq!(x.len(), 5);
+}
+
+#[test]
+fn clone_nonliteral() {
+    let mut y = Some(42);
+    let x = avec!(y.take().unwrap(); 3);
+    assert_eq!(x.len(), 3);
 }
